999c542c8d0bf401b04b8bacde8ae5f0d06ab00d
pnaveenvm <72395722+pnaveenvm@users.noreply.github.com>
2022-08-01 12:24:02 +0530

    added new files

---
+++ src/parent.js
@@ -0,0 +1,10 @@
+import React,{useState} from "react";
+
+const Parent = () =>{
+    const [count, setCount] = useState(0);
+    return(
+        <>ParentComponet</>
+    )
+}
+
+export default Parent;
