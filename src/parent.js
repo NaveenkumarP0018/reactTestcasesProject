53d6b470da1cd00d5cc1091f4017380f2cd4e8b1
pnaveenvm <72395722+pnaveenvm@users.noreply.github.com>
2022-08-11 12:08:25 +0530

    added searchcode

--- src/parent.js
+++ src/parent.js
@@ -1,9 +1,45 @@
-import React,{useState} from "react";
+import React, { useState, useEffect } from "react";
+import Table from 'react-bootstrap/Table';
+const Parent = () => {
+    const [count, setCount] = useState([]);
+    const [searchedValue,setSearchedValue] = useState('');
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(resp => resp.json()).then(json => setCount(json))
 
-const Parent = () =>{
-    const [count, setCount] = useState(0);
-    return(
-        <>ParentComponet</>
+    }, []);
+    console.log(searchedValue);
+    let filteredVlues = searchedValue !== '' ? count.filter((x)=>x.name.toLowerCase().includes(searchedValue.toLowerCase())):count;
+   
+    return (
+        <div>
+            <input placeholder="search username" onChange={(e)=>setSearchedValue(e.target.value)}/>
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>userName</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        filteredVlues.map((x,index)=>{
+                            return <tr>
+                            <td>{x.name}</td>
+                            <td>{x.username}</td>
+                            </tr>
+                        })
+                    }
+                    {/* <tr>
+                        <td>1</td>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                    </tr> */}
+                    
+                </tbody>
+            </Table>
+        </div>
     )
 }
 
